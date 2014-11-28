d5edec893d129ebbf3c5eb36a293c1f7cb41d5d2
takahiro-kosaka <takahiro.kosaka@dena.com>
2014-11-28 21:29:22 +0900

    blur

--- test.cs
+++ test.cs
@@ -73,9 +73,8 @@ public class test : SetupLux {
 		//cam.RenderToCubemap (this.specularCube, faceMask);
 		 //this.diffuseCube.hideFlags = HideFlags.HideAndDontSave;
 		cam.RenderToCubemap (rtex, faceMask);
-		
-		rtex.SmoothEdges(4);
-
+		//rtex.mipMapBias = 3.5f;
+		//rtex.SmoothEdges (8);
 		/*
 		if (!diff) {
 			diff = rtex;
@@ -119,20 +118,35 @@ public class test : SetupLux {
 					ResetPixel();
 
 					//Right side of pixel
-					for (x = xx; (x < xx + blurSize && x < _W); ++x) {
-						AddPixel(image.GetPixel(face, x, yy));
+					for (x = xx; (x < xx + blurSize); ++x) {
+						if (x < _W) {
+							AddPixel(image.GetPixel(face, x, yy));
+						} else { // over right pixel
+							int index_x = x - _W;
+							int index_y = yy;
+							Color col = getOverRightPixel(image, face, index_x, index_y);
+							//Color col = new Color(0,0,255);
+							AddPixel(col);
+						}
 					}
 
 					//Left side of pixel
-					for (x = xx; (x > xx - blurSize && x > 0); --x) {
-						AddPixel(image.GetPixel( face, x, yy));
+					for (x = xx; (x > xx - blurSize); --x) {
+						if (x < 0) {
+							int index_x = 0 - x;
+							int index_y = yy;
+							Color col = getOverLeftPixel(image, face, index_x, index_y);
+							AddPixel(col);
+						} else {// over left pixel
+							AddPixel(image.GetPixel( face, x, yy));
+						}
 					}
 
 					CalcPixel();
 
-					for (x = xx; x < xx + blurSize && x < _W; ++x) {
-						blurred.SetPixel(face, x, yy, new Color(avgR, avgG, avgB, 1.0f));
-					}
+					//for (x = xx; x < xx + blurSize && x < _W; ++x) {
+						blurred.SetPixel(face, xx, yy, new Color(avgR, avgG, avgB, 1.0f));
+					//}
 				}
 			}
 		} else {
@@ -141,20 +155,34 @@ public class test : SetupLux {
 					ResetPixel();
 
 					// Over pixel
-					for (y = yy; (y < yy + blurSize && y < _H); ++y) {
-						AddPixel(image.GetPixel(face, xx, y));
+					for (y = yy; (y < yy + blurSize); ++y) {
+						if (y < _H) {
+							AddPixel(image.GetPixel(face, xx, y));
+						} else {
+							int index_x = xx;
+							int index_y = y - _H;
+							Color col = getOverTopPixel(image, face, index_x, index_y);
+							AddPixel(col);
+						}
 					}
 
 					// Under pixel
-					for (y = yy; (y < yy + blurSize && y < 0); --y) {
-						AddPixel(image.GetPixel(face, xx, y));
+					for (y = yy; (y < yy + blurSize); --y) {
+						if (y < 0) {
+							int index_x = xx;
+							int index_y = 0 - y;
+							Color col = getOverBottomPixel(image, face, index_x, index_y);
+							AddPixel(col);
+						} else {
+							AddPixel(image.GetPixel(face, xx, y));
+						}
 					}
 
 					CalcPixel();
 
-					for (y = yy; (y < yy + blurSize && y < _H); ++y) {
-						blurred.SetPixel( face, xx, y, new Color(avgR, avgG, avgB, 1.0f));
-					}
+					//for (y = yy; (y < yy + blurSize && y < _H); ++y) {
+						blurred.SetPixel( face, xx, yy, new Color(avgR, avgG, avgB, 1.0f));
+					//}
 				}
 			}
 		}
@@ -185,5 +213,137 @@ public class test : SetupLux {
 		avgG =0;
 		avgB =0;
 		*/
+	}
+
+	Color getOverRightPixel(Cubemap image, CubemapFace face, int index_x, int index_y) {
+		Color pixCol;
+		CubemapFace targetFace = CubemapFace.NegativeX;
+		int x=0, y=0, axis;  // axis= +-1(x) +-2(y)
+		if (face == CubemapFace.PositiveX) {
+			targetFace = CubemapFace.NegativeZ;
+			x = index_x;
+			y = index_y;
+		} else if (face == CubemapFace.PositiveZ) {
+			targetFace = CubemapFace.PositiveX;
+			x = index_x;
+			y = index_y;
+		} else if (face == CubemapFace.NegativeZ) {
+			targetFace = CubemapFace.NegativeX;
+			x = index_x;
+			y = index_y;
+		} else if (face == CubemapFace.NegativeX) {
+			targetFace = CubemapFace.PositiveZ;
+			x = index_x;
+			y = index_y;
+		} else if (face == CubemapFace.PositiveY) {
+			targetFace = CubemapFace.PositiveX;
+			x = cubemapSize - index_y;
+			y = index_x;
+		} else if (face == CubemapFace.NegativeY) {
+			targetFace = CubemapFace.PositiveX;
+			x = index_y;
+			y = cubemapSize - index_x;
+		} 
+		pixCol = image.GetPixel (targetFace, x, y);
+		return pixCol;
+	}
+	Color getOverLeftPixel(Cubemap image, CubemapFace face, int index_x, int index_y) {
+		Color pixCol;
+		CubemapFace targetFace = CubemapFace.NegativeX;
+		int x=0, y=0, axis;  // axis= +-1(x) +-2(y)
+		if (face == CubemapFace.PositiveX) {
+			targetFace = CubemapFace.PositiveZ;
+			x = cubemapSize - index_x;
+			y = index_y;
+		} else if (face == CubemapFace.PositiveZ) {
+			targetFace = CubemapFace.NegativeX;
+			x = cubemapSize - index_x;
+			y = index_y;
+		} else if (face == CubemapFace.NegativeZ) {
+			targetFace = CubemapFace.PositiveX;
+			x = cubemapSize - index_x;
+			y = index_y;
+		} else if (face == CubemapFace.NegativeX) {
+			targetFace = CubemapFace.NegativeZ;
+			x = cubemapSize - index_x;
+			y = index_y;
+		} else if (face == CubemapFace.PositiveY) {
+			targetFace = CubemapFace.NegativeX;
+			x = index_y;
+			y = index_x;
+		} else if (face == CubemapFace.NegativeY) {
+			targetFace = CubemapFace.NegativeX;
+			x = index_y;
+			y = cubemapSize - index_x;
+		} 
+		pixCol = image.GetPixel (targetFace, x, y);
+		//pixCol = new Color (0, 0, 200);
+		return pixCol;
+	}
+	Color getOverTopPixel(Cubemap image, CubemapFace face, int index_x, int index_y) {
+		Color pixCol;
+		CubemapFace targetFace = CubemapFace.NegativeX;
+		int x=0, y=0, axis;  // axis= +-1(x) +-2(y)
+		if (face == CubemapFace.PositiveX) {
+			targetFace = CubemapFace.NegativeY;
+			x = cubemapSize - index_y;
+			y = index_x;
+		} else if (face == CubemapFace.PositiveZ) {
+			targetFace = CubemapFace.NegativeY;
+			x = index_x;
+			y = index_y;
+		} else if (face == CubemapFace.NegativeZ) {
+			targetFace = CubemapFace.PositiveY;
+			x = cubemapSize - index_x;
+			y = cubemapSize - index_y;
+		} else if (face == CubemapFace.NegativeX) {
+			targetFace = CubemapFace.NegativeY;
+			x = cubemapSize - index_y;
+			y = cubemapSize - index_x;
+		} else if (face == CubemapFace.PositiveY) {
+			targetFace = CubemapFace.NegativeZ;
+			x = index_x;
+			y = cubemapSize - index_y;
+		} else if (face == CubemapFace.NegativeY) {
+			targetFace = CubemapFace.PositiveZ;
+			x = index_x;
+			y = index_y;
+		} 
+		pixCol = image.GetPixel (targetFace, x, y);
+		//pixCol = new Color (0, 0, 200);
+		return pixCol;
+	}
+	Color getOverBottomPixel(Cubemap image, CubemapFace face, int index_x, int index_y) {
+		Color pixCol;
+		CubemapFace targetFace = CubemapFace.NegativeX;
+		int x=0, y=0, axis;  // axis= +-1(x) +-2(y)
+		if (face == CubemapFace.PositiveX) {
+			targetFace = CubemapFace.PositiveY;
+			x = cubemapSize - index_y;
+			y = index_x;
+		} else if (face == CubemapFace.PositiveZ) {
+			targetFace = CubemapFace.PositiveY;
+			x = index_x;
+			y = index_y;
+		} else if (face == CubemapFace.NegativeZ) {
+			targetFace = CubemapFace.NegativeY;
+			x = cubemapSize - index_x;
+			y = cubemapSize - index_y;
+		} else if (face == CubemapFace.NegativeX) {
+			targetFace = CubemapFace.PositiveY;
+			x = cubemapSize - index_y;
+			y = cubemapSize - index_x;
+		} else if (face == CubemapFace.PositiveY) {
+			targetFace = CubemapFace.PositiveZ;
+			x = index_x;
+			y = cubemapSize - index_y;
+		} else if (face == CubemapFace.NegativeY) {
+			targetFace = CubemapFace.NegativeZ;
+			x = index_x;
+			y = index_y;
+		} 
+		pixCol = image.GetPixel (targetFace, x, y);
+		//pixCol = new Color (0, 0, 200);
+		return pixCol;
 	}
 }
