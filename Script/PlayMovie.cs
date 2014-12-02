786a8341f542c6edf08705a76f413dd7f2c4effa
takahiro-kosaka <takahiro.kosaka@dena.com>
2014-12-02 22:24:47 +0900

    add movie

---
+++ Script/PlayMovie.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MovieTexture tex = (MovieTexture)renderer.material.mainTexture;
+		tex.loop = true;
+		tex.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Script/WebCamBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebCamBehaviourScript : MonoBehaviour
+{
+	public int Width = 1920;
+	public int Height = 1080;
+	public int FPS = 30;
+	
+	// Use this for initialization
+	void Start()
+	{
+		var devices = WebCamTexture.devices;
+		if ( devices.Length == 0 ) {
+			Debug.LogError( "Webカメラが検出できませんでした。" );
+			return;
+		}
+		
+		// WebCamテクスチャを作成する
+		var webcamTexture = new WebCamTexture( Width, Height, FPS );
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
